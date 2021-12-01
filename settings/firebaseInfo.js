c849d30edef9637c7919b24a4ab624978b3e1d72
Q-onderzoek <89088143+Q-onderzoek@users.noreply.github.com>
2021-12-01 11:33:35 +0100

    j
    j

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyAOM3CAxNsBGm9HSVcbGg0km6C_kyNFwv0",
-    authDomain: "qonderzoek.firebaseapp.com",
-    databaseURL: "https://qonderzoek-default-rtdb.firebaseio.com",
-    projectId: "qonderzoek",
-    storageBucket: "qonderzoek.appspot.com",
-    messagingSenderId: "167026780855",
-    appId: "1:167026780855:web:a1754f3a04c34344f5fb32",
-    measurementId: "G-88Y9RXSMC4"
+  apiKey: "AIzaSyBguXvt-9uk81-w4V9O1nARY-rC2xAQAWI",
+  authDomain: "bachelorproject-a0528.firebaseapp.com",
+  databaseURL: "https://bachelorproject-a0528-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bachelorproject-a0528",
+  storageBucket: "bachelorproject-a0528.appspot.com",
+  messagingSenderId: "4209011194",
+  appId: "1:4209011194:web:4b9a21a86d0dc9d8c89c43",
+  measurementId: "G-DT3JTM04YH"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
