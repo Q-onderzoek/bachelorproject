f1d95a2014027d38261a865f07feca616c80d006
Q-onderzoek <89088143+Q-onderzoek@users.noreply.github.com>
2021-12-01 13:24:11 +0100

    g
    g

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBguXvt-9uk81-w4V9O1nARY-rC2xAQAWI",
-  authDomain: "bachelorproject-a0528.firebaseapp.com",
-  databaseURL: "https://bachelorproject-a0528-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bachelorproject-a0528",
-  storageBucket: "bachelorproject-a0528.appspot.com",
-  messagingSenderId: "4209011194",
-  appId: "1:4209011194:web:4b9a21a86d0dc9d8c89c43",
-  measurementId: "G-DT3JTM04YH"
+  apiKey: "AIzaSyBuVfV6Aoiu6neNd7lyqIEb4hGJkRLaJAE",
+  authDomain: "bep2021-829c3.firebaseapp.com",
+  databaseURL: "https://bep2021-829c3-default-rtdb.firebaseio.com",
+  projectId: "bep2021-829c3",
+  storageBucket: "bep2021-829c3.appspot.com",
+  messagingSenderId: "507590890332",
+  appId: "1:507590890332:web:126065f81bbf91cf7f2799",
+  measurementId: "G-7SHQ6FB3M6"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
