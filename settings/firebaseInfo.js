2821625d67594d457aeaa6b1909aabf6d922a49d
Maarten Kroesen - TBM <maartenkroesen@tudelft.nl>
2022-11-28 13:51:46 +0100

    t
    t

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,14 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBuVfV6Aoiu6neNd7lyqIEb4hGJkRLaJAE",
-  authDomain: "bep2021-829c3.firebaseapp.com",
-  databaseURL: "https://bep2021-829c3-default-rtdb.firebaseio.com",
-  projectId: "bep2021-829c3",
-  storageBucket: "bep2021-829c3.appspot.com",
-  messagingSenderId: "507590890332",
-  appId: "1:507590890332:web:126065f81bbf91cf7f2799",
-  measurementId: "G-7SHQ6FB3M6"
+  apiKey: "AIzaSyBtNGcJLbrhzswf-EH5Z4tw1Ie24lRrp1w",
+  authDomain: "bep2022-6334d.firebaseapp.com",
+  databaseURL: "https://bep2022-6334d-default-rtdb.firebaseio.com",
+  projectId: "bep2022-6334d",
+  storageBucket: "bep2022-6334d.appspot.com",
+  messagingSenderId: "958536690125",
+  appId: "1:958536690125:web:571516c1ea42266a077c84",
+  measurementId: "G-WSBGLVGS9C"
+
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
